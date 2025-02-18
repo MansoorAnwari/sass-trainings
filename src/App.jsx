ae99deba9140707ed611768fa3f86e975941d5b1
Mansoor Anwari <mansooranwary50@gmail.com>
2025-02-18 11:46:05 +0330

    feat(docs): update README.md content

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <div>
+            <button className="button">Primary Button</button>
+            <button className="button-danger">Danger Button</button>
+            <button className="button-secondary">Secondary Button</button>
+        </div>
+    );
+}
+
+export default App;
