f7891a659528cfa2b5063b1e189bee61814949d7
Mansoor Anwari <mansooranwary50@gmail.com>
2025-02-21 19:44:51 +0330

    feat(structure): organize project with components, pages, and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@
 function App() {
     return (
         <div>
-            <button className="button">Primary Button</button>
-            <button className="button-danger">Danger Button</button>
-            <button className="button-secondary">Secondary Button</button>
         </div>
     );
 }
